0e34199cb75b1df2f2e3ff425c5cd480b43d9e61
hallmao <michael.brochonski@gmail.com>
2014-07-11 18:54:12 -0300

    Resposta Transitória e Natural em Variaveis Separadas
    Resposta Transitória e Natural em Variaveis Separadas

--- Segunda ORDEM Finais/Solving_order_2_ode_2.py
+++ Segunda ORDEM Finais/Solving_order_2_ode_2.py
@@ -1,6 +1,8 @@
-from sympy import Function, dsolve, pprint, exp, cos
+﻿from sympy import Function, dsolve, pprint, exp, cos
 from sympy.abc import t
 from sympy import *
+
+#C1,C2 = symbols("C1 C2")
 
 
 ##Loop para testes
@@ -26,9 +28,36 @@ while True:
     eq =sympify(a2*y(t).diff(t,2) + a1*y(t).diff(t) +a0*y(t) -xT)
 
     pprint(eq)
+    print ""
 
     ###Dif equation solver
     solvedEq = dsolve(sympify(eq),y(t),hint='nth_linear_constant_coeff_undetermined_coefficients')
+
+    ##Transformação do tipo sympy_unity para o sympy_mul (mais operações permitidas)
+    sepEq = solvedEq._args[1]
+
+    ##PRocesso de separação de resp natural e resposta transitória
+    elementosEq = sepEq.atoms(Symbol)
+    C1 = elementosEq.pop()
+    C2 = elementosEq.pop()
+
+    print C1,C2
+
+    if C2 != t :
+        print "Tem c2"
+        RespTran =  sepEq.subs([(C2,0),(C1,0)])
+        
+        
+    else:
+        RespTran =  sepEq.subs(C1,0)
+        
+    ##Resposta transitória alocada em  RespTran, natural em RespNat
+
+    RespNat  =  sepEq.subs(RespTran,0)
+
+    print RespTran
+    print RespNat
+
 
     ##Saida
     print(sympify(solvedEq, rational = False , evaluate = False).evalf(2))
